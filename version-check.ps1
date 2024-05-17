db1016daa8936c075a556e0051e20d6b6256f750
Malbios <no@mail>
2024-05-17 20:24:19 +0200

    better versioning

--- version-check.ps1
+++ version-check.ps1
@@ -1,3 +1,10 @@
+git config user.name "github-actions"
+git config user.email "github-actions@github.com"
+
+git fetch
+git checkout dev
+git pull
+
 $onlineVersion = (npm view moo-client-ts version)
 $localVersion = (npm pkg get version)
 
@@ -7,5 +14,7 @@ $onlineVersion = [System.Version]$onlineVersion
 $localVersion = [System.Version]$localVersion
 
 if ($localVersion -le $onlineVersion) {
-    throw "local version '$($localVersion)' is not higher than online version '$($onlineVersion)'"
+    # throw "local version '$($localVersion)' is not higher than online version '$($onlineVersion)'"
+    npm version minor
+    git push --tags origin HEAD:dev
 }
