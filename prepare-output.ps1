b706d080deadf6919e7581355e8a53a6ae1fd088
Malbios <no@mail>
2024-05-17 22:18:42 +0200

    build process refactor

--- prepare-output.ps1
+++ prepare-output.ps1
@@ -1,6 +1,5 @@
 mkdir ./dist/lib | Out-Null
-Copy-Item -Recurse ./dist/src/* ./dist/lib | Out-Null
-Remove-Item -Recurse ./dist/src | Out-Null
+Move-Item ./dist/* -Exclude lib ./dist/lib | Out-Null
 Copy-Item ./package.json ./dist | Out-Null
 Copy-Item ./README.md ./dist | Out-Null
 Copy-Item ./LICENSE ./dist | Out-Null
