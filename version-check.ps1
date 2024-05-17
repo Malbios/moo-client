2331d5e6d6af7e9407a52c451714c1fbc26c56ad
Malbios <no@mail>
2024-05-17 22:43:05 +0200

    version check adjustment

--- version-check.ps1
+++ version-check.ps1
@@ -13,6 +13,9 @@ $localVersion = $localVersion -replace "`"", ""
 $onlineVersion = [System.Version]$onlineVersion
 $localVersion = [System.Version]$localVersion
 
+Write-Host "Online Version: $($onlineVersion)"
+Write-Host "Local Version: $($localVersion)"
+
 if ($localVersion -le $onlineVersion) {
     # throw "local version '$($localVersion)' is not higher than online version '$($onlineVersion)'"
     npm version minor
